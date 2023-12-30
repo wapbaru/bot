c8022146f90c5bf22a56abb5ecbb551fc8a3b380
Felyxh <155202658+Felyxh@users.noreply.github.com>
2023-12-30 20:36:21 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+.
+$ apt update
+
+.
+$ apt upgrade
+
+.
+$ pkg install git
+
+.
+$ git clone https://github.com/Felyxh/Tools
+
+.
+$ cd ded
+
+.
+$ pip install requests
+
+.
+$ pip2 install requests
+
+.
+$ pip2 install mechanize
+
+.
+$ chmod +x *
+
+.
+$ sh new.sh
+
+
---
+++ new.sh
@@ -0,0 +1,98 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     INDONESIA CYBER SGB TEAM "$i"        |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" DEDI ROHADI  "$i"          |"
+echo $i" |"$pu" Contact WhatsApp :"$ku" 087774444221"$i" |"
+echo $i" |"$pu" Contact Email :"$ku" wwx@dr.com"$i"      |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me"1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|"$i"    |"$cy" D"$i" |"
+echo $i"|"$me"2"$i" |"$cy" BRUTEFORCE FB SGB team   "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"3"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" D"$i" |"
+echo $i"|"$me"4"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" I"$i" |"
+echo $i"|"$me"5"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|"$i"    |"$cy" #"$i" |"
+echo $i"|"$me"6"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" #"$i" |"
+echo $i"|"$me"7"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" #"$i" |"
+echo $i"|"$me"8"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" R"$i" |"
+echo $i"|"$me"9"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"    |"$cy" O"$i" |"
+echo $i"|"$me"10"$i" |"$cy" PHISING FACEBOOK 3      "$i"|"$i"    |"$cy" H"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" A"$i" |"
+echo $i"|"$me"11"$i" |"$cy" INSTALL BAHAN DULU      "$i"|"$i"    |"$cy" D"$i" |"
+echo $i"|"$me"12"$i" |"$cy" KELUAR/EXIT PROGRAM     "$i"|"$i"    |"$cy" I"$i" |"
+echo $i"────────────────────────────────    ─────"
+echo
+echo $me"┌==="$bi"["$i"Silahkan kamu"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/
+cd fb-brute
+python2 brute.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Felyxh/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+
+if [ $pil = 11 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" 087774444221"
+sleep 2
+echo $pur">> Thanks Yang Sudah Support Saya <<"
+exit
+fi
