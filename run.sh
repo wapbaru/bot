c35617dd1cc1ef9bbd3cc42d00796ea378c8b3b2
Dedi Rohadi <155202658+Felyxh@users.noreply.github.com>
2023-12-31 19:19:12 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ clear
 bi='\033[34;1m' #biru
 i='\033[32;1m' #ijo
 pur='\033[35;1m' #purple
-de='\033[36;1m' #putih
+cy='\033[36;1m' #putih
 me='\033[31;1m' #merah
 pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
-
+# Jangan Recode Ya gayn
 echo
 echo
 echo
@@ -22,36 +22,34 @@ echo $i" |"$pu" Contack WhatsApp :"$ku" 085835787069"$i"             |"
 echo $i" |"$pu" Contack Gmail :"$ku" dermawan0858@gmail.com"$i"      |"
 echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
 echo
-
-
-echo $i"────────────────────────────────"    
-echo $i"|"$me" 1"$i" |"$de" BRUTEFORCE FB PHP        "$i"|"$i"    
-echo $i"|"$me" 2"$i" |"$de" BRUTEFORCE FB BR1G4D3    "$i"|"$i"    
-echo $i"|"$me" 3"$i" |"$de" BRUTEFORCE FB iqbalz     "$i"|"$i"    
-echo $i"|"$me" 4"$i" |"$de" BRUTEFORCE FB MBF        "$i"|"$i"    
-echo $i"|"$me" 5"$i" |"$de" FACEBRUTE N1ght420       "$i"|"$i"    
-echo $i"|"$me" 6"$i" |"$de" BRUTEFORCE FB ( root )   "$i"|"$i"    
-echo $i"────────────────────────────────"$i"
-echo $i"|"$me" 7"$i" |"$de" YAHOO CLONING FACEBOOK   "$i"|"$i"   
-echo $i"|"$me" 8"$i" |"$de" PROFILE GUARD FACEBOOK   "$i"|"$i"    
-echo $i"|"$me" 9"$i" |"$de" REPORT FACEBOOK WORK     "$i"|"$i"    
-echo $i"|"$me"10"$i" |"$de" AUTO REACTION FACEBOOK   "$i"|"$i"    
-echo $i"|"$me"11"$i" |"$de" INFORMATION FACEBOOK     "$i"|"$i"    
-echo $i"|"$me"12"$i" |"$de" BOT KOMEN FACEBOOK       "$i"|"$i"    
-echo $i"|"$me"13"$i" |"$de" AUTOLIKE FACEBOOK        "$i"|"$i"    
-echo $i"────────────────────────────────"$i"    
-echo $i"|"$me"14"$i" |"$de" PHISING FACEBOOK 1       "$i"|"$i"  
-echo $i"|"$me"15"$i" |"$de" PHISING FACEBOOK 2       "$i"|"$i"   
-echo $i"|"$me"16"$i" |"$de" PHISING FACEBOOK 3       "$i"|"$i"    
-echo $i"|"$me"17"$i" |"$de" PHISING FACEBOOK 4       "$i"|"$i"    
-echo $i"────────────────────────────────"$i"    
-echo $i"|"$me"18"$i" |"$de" INSTALL BAHAN DULU COEG  "$i"|"$i"    
-echo $i"|"$me"19"$i" |"$de" KELUAR/EXIT PROGRAM      "$i"|"$i"    
-echo $i"────────────────────────────────"
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3    "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE FB iqbalz     "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|"$i"    |"$cy" G"$i" |"
+echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB ( root )   "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" L"$i" |"
+echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|"$i"    ─────"
+echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" F"$i" |"
+echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me"10"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|"$i"    |"$cy" C"$i" |"
+echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"12"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" O"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" O"$i" |"
+echo $i"|"$me"14"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"    ─────"
+echo $i"|"$me"16"$i" |"$cy" PHISING FACEBOOK 3       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me"17"$i" |"$cy" PHISING FACEBOOK 4       "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" R"$i" |"
+echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"────────────────────────────────    ─────"
 echo
-echo $me" "$bi"["$i"silahkan"$bi"]"$me"======"$bi"["$i""masukan Nomornya""$bi"]"
+echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
 echo $me"¦"
-read -p" [ ]# " pil
+read -p"└──# " pil
 
 
 
