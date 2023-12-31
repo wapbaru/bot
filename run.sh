5b69862c79cdb6cbe1ff2ac0fb058f185fef4a43
Dedi Rohadi <155202658+Felyxh@users.noreply.github.com>
2023-12-31 19:31:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,35 +7,22 @@ cy='\033[36;1m' #putih
 me='\033[31;1m' #merah
 pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
-# Jangan Recode Ya gayn
+
 echo
 echo
 echo
 
-echo $i"────────────────────────────────    ─────"
-echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|"$i"    
-echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3    "$i"|"$i"    
-echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE FB iqbalz     "$i"|"$i"    |"$cy" N"$i" |"
-echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|"$i"    |"$cy" G"$i" |"
-echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420       "$i"|"$i"    |"$cy" K"$i" |"
-echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB ( root )   "$i"|"$i"    |"$cy" E"$i" |"
-echo $i"────────────────────────────────"$i"    |"$cy" L"$i" |"
-echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|"$i"    ─────"
-echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" F"$i" |"
-echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" A"$i" |"
-echo $i"|"$me"10"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|"$i"    |"$cy" C"$i" |"
-echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" E"$i" |"
-echo $i"|"$me"12"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" B"$i" |"
-echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" O"$i" |"
-echo $i"────────────────────────────────"$i"    |"$cy" O"$i" |"
-echo $i"|"$me"14"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"    |"$cy" K"$i" |"
-echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"    ─────"
-echo $i"|"$me"16"$i" |"$cy" PHISING FACEBOOK 3       "$i"|"$i"    |"$cy" K"$i" |"
-echo $i"|"$me"17"$i" |"$cy" PHISING FACEBOOK 4       "$i"|"$i"    |"$cy" E"$i" |"
-echo $i"────────────────────────────────"$i"    |"$cy" R"$i" |"
-echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    |"$cy" E"$i" |"
-echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" N"$i" |"
-echo $i"────────────────────────────────    ─────"
+
+
+echo $i"────────────────────────────────"$i"    "$cy" "$i" "
+echo $i"|"$me"1"$i" |"$cy" REPORT TIKTOK       "$i"|"$i"    "$cy" "$i" "
+echo $i"|"$me"2"$i" |"$cy" REPORT INSTAGRAM    "$i"|"$i"     "$cy" "$i"
+echo $i"|"$me"3"$i" |"$cy" REPORT FACEBOOK     "$i"|"$i"    "$cy" "$i" "
+echo $i"|"$me"4"$i" |"$cy" LACAK USER          "$i"|"$i"    "$cy" "$i" "
+echo $i"────────────────────────────────"$i"    "$cy" "$i" "
+echo $i"|"$me"01"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    "$cy" "$i" "
+echo $i"|"$me"02"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    "$cy" "$i" "
+echo $i"────────────────────────────────"
 echo
 echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
 echo $me"¦"
@@ -43,7 +30,7 @@ read -p"└──# " pil
 
 
 
-if [ $pil = 3 ]
+if [ $pil = 1 ]
 then
 clear
 figlet -f slant "W A I T"|lolcat
@@ -54,7 +41,7 @@ npm i
 node .
 fi
 
-if [ $pil = 4 ]
+if [ $pil = 3 ]
 then
 clear
 figlet -f slant "W A I T"|lolcat
@@ -65,7 +52,7 @@ unzip Report.zip
 python2 Report.py
 fi
 
-if [ $pil = 9 ]
+if [ $pil = 4 ]
 then
 clear
 figlet -f slant "W A I T"|lolcat
@@ -75,7 +62,7 @@ cd ip
 python ip.py  
 fi
 
-if [ $pil = 11 ]
+if [ $pil = 01 ]
 then
 clear
 apt update && apt upgrade
@@ -95,7 +82,7 @@ pkg install nano
 figlet -f slant " S U K S E S "|lolcat
 fi
 
-if [ $pil = 12 ]
+if [ $pil = 02 ]
 then
 clear
 figlet -f slant "E X I T"|lolcat
